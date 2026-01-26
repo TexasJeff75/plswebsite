90b4c99469719b14d56ad0ed8ac2f14fbeb737be
TexasJeff75 <texasjeff75@gmail.com>
2026-01-26 16:47:48 -0600

    Updated tailwind.config.js

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,17 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
   build: {
-    outDir: 'dist'
-  },
-  server: {
-    port: 5173
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        about: 'about.html',
+        contact: 'contact.html',
+        compliance: 'compliance.html',
+        services: 'services.html',
+        'why-proximity': 'why-proximity.html'
+      }
+    }
   }
 });
